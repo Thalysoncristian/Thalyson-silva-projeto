046f6784c7ddeaf3665eb14c776eb3e7f8a51b01
Thalysoncristian <thalysoncristian19@gmail.com>
2024-12-03 17:19:56 -0300

    update script

--- script.js
+++ script.js
@@ -7,73 +7,99 @@ function adicionarGabinete() {
     gabineteDiv.className = 'gabinete';
     gabineteDiv.id = `gabineteDiv${numeroGabinetes}`;
     gabineteDiv.innerHTML = `
-        <label for="gabinete${numeroGabinetes}">*INFORMAR GABINETE - ${numeroGabinetes}:*</label>
-        <select id="gabinete${numeroGabinetes}" required>
-            <option value="DELTA">DELTA</option>
-            <option value="DELTA ORION">DELTA ORION</option>
-            <option value="DELTA TX">DELTA TX</option>
-            <option value="ELTEK">ELTEK</option>
-            <option value="ELTEK FULL TX">ELTEK FULL TX</option>
-            <option value="EMERSON">EMERSON</option
-            <option value="EMERSON TX">EMERSON TX</option>
-            <option value="PHB">PHB</option>
-        </select>
-
-        <label for="retificadores${numeroGabinetes}">*QUANTIDADE DE RETIFICADORES POR GABINETE-FCC:*</label>
-        <input type="number" id="retificadores${numeroGabinetes}" required>
-
-        <label for="baterias${numeroGabinetes}">*QUANTIDADE DE BATERIAS NO GABINETE-FCC:*</label>
-        <input type="number" id="baterias${numeroGabinetes}" required>
-
-        <label for="siteBateria${numeroGabinetes}">*SITE COM BATERIA:*</label>
-        <select id="siteBateria${numeroGabinetes}" required>
-            <option value="SIM">SIM</option>
-            <option value="NAO">NÃO</option>
-        </select>
-
-        <label for="semAutonomia${numeroGabinetes}">*SEM AUTONOMIA:*</label>
-        <input type="checkbox" id="semAutonomia${numeroGabinetes}">
-
-        <label for="infoBateria${numeroGabinetes}">*INFORMAÇÕES DA BATERIA:*</label>
-        <select id="infoBateria${numeroGabinetes}" required>
-            <option value="EP TELECOM">EP TELECOM</option>
-            <option value="HUAWEI">HUAWEI</option>
-            <option value="MOURA">MOURA</option>
-            <option value="NEWMAX">NEWMAX</option>
-            <option value="UNIPOWER">UNIPOWER</option>
-            <option value="ZTE">ZTE</option>
-        </select>
-
-        <label for="quantidadeBancos${numeroGabinetes}">*QUANTIDADE DE BANCOS:*</label>
-        <input type="number" id="quantidadeBancos${numeroGabinetes}" required>
-
-        <label for="capacidade${numeroGabinetes}">*CAPACIDADE:*</label>
-        <input type="text" id="capacidade${numeroGabinetes}" required>
-
-        <label for="volts${numeroGabinetes}">*VOLTS:*</label>
-        <select id="volts${numeroGabinetes}" required>
-            <option value="2 V">2 V</option>
-            <option value="12 V">12 V</option>
-            <option value="24 V">24 V</option>
-            <option value="48 V">48 V</option>
-        </select>
-
-        <label for="elemento${numeroGabinetes}">*ELEMENTO:*</label>
-        <input type="text" id="elemento${numeroGabinetes}" required>
-
-        <label for="consumoFonte${numeroGabinetes}">*CONSUMO FONTE:*</label>
-        <input type="text" id="consumoFonte${numeroGabinetes}" required>
-
-        <button type="button" onclick="removerGabinete(${numeroGabinetes})">Remover Gabinete</button>
+        <div class="form-group">
+            <label for="gabinete${numeroGabinetes}">INFORMAR GABINETE - ${numeroGabinetes}:</label>
+            <select id="gabinete${numeroGabinetes}" class="form-control" required>
+                <option value="DELTA">DELTA</option>
+                <option value="DELTA ORION">DELTA ORION</option>
+                <option value="DELTA TX">DELTA TX</option>
+                <option value="ELTEK">ELTEK</option>
+                <option value="ELTEK FULL TX">ELTEK FULL TX</option>
+                <option value="EMERSON">EMERSON</option>
+                <option value="EMERSON TX">EMERSON TX</option>
+                <option value="PHB">PHB</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="retificadores${numeroGabinetes}">QUANTIDADE DE RETIFICADORES NO GABINETE ${numeroGabinetes}-FCC:</label>
+            <input type="number" id="retificadores${numeroGabinetes}" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="siteBateria${numeroGabinetes}">SITE COM BATERIA:</label>
+            <select id="siteBateria${numeroGabinetes}" class="form-control" onchange="mostrarBateriaInfo(${numeroGabinetes})" required>
+                <option value="SIM">SIM</option>
+                <option value="NAO">NÃO</option>
+            </select>
+        </div>
+        <div class="bateria-info" id="bateriaInfo${numeroGabinetes}" style="display: none;">
+            <div class="form-group">
+                <label for="baterias${numeroGabinetes}">QUANTIDADE DE BATERIAS NO GABINETE-FCC ${numeroGabinetes}:</label>
+                <input type="number" id="baterias${numeroGabinetes}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="infoBateria${numeroGabinetes}">INFORMAÇÕES DA BATERIA:</label>
+                <select id="infoBateria${numeroGabinetes}" class="form-control">
+                    <option value="EP TELECOM">EP TELECOM</option>
+                    <option value="HUAWEI">HUAWEI</option>
+                    <option value="MOURA">MOURA</option>
+                    <option value="NEWMAX">NEWMAX</option>
+                    <option value="UNIPOWER">UNIPOWER</option>
+                    <option value="ZTE">ZTE</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="quantidadeBancos${numeroGabinetes}">QUANTIDADE DE BANCOS:</label>
+                <input type="number" id="quantidadeBancos${numeroGabinetes}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="capacidade${numeroGabinetes}">CAPACIDADE:</label>
+                <input type="text" id="capacidade${numeroGabinetes}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="volts${numeroGabinetes}">VOLTS:</label>
+                <select id="volts${numeroGabinetes}" class="form-control">
+                    <option value="2 V">2 V</option>
+                    <option value="12 V">12 V</option>
+                    <option value="24 V">24 V</option>
+                    <option value="48 V">48 V</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="elemento${numeroGabinetes}">ELEMENTO:</label>
+                <input type="number" id="elemento${numeroGabinetes}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="consumoFonte${numeroGabinetes}">CONSUMO FONTE:</label>
+                <input type="number" id="consumoFonte${numeroGabinetes}" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="autonomia${numeroGabinetes}">AUTONOMIA:</label>
+                <select id="autonomia${numeroGabinetes}" class="form-control" onchange="mostrarTempoAutonomia(${numeroGabinetes})" required>
+                    <option value="SIM">SIM</option>
+                    <option value="NAO">NÃO</option>
+                </select>
+            </div>
+            <div class="form-group" id="tempoAutonomiaGroup${numeroGabinetes}" style="display: none;">
+                <label for="tempoAutonomia${numeroGabinetes}">TEMPO DE AUTONOMIA:</label>
+                <input type="text" id="tempoAutonomia${numeroGabinetes}" class="form-control">
+            </div>
+        </div>
     `;
-
     gabinetesContainer.appendChild(gabineteDiv);
 }
 
-// Função para remover gabinetes
-function removerGabinete(numero) {
-    const gabineteDiv = document.getElementById(`gabineteDiv${numero}`);
-    gabineteDiv.remove();
+// Função para mostrar ou esconder informações da bateria
+function mostrarBateriaInfo(numeroGabinetes) {
+    const siteBateria = document.getElementById(`siteBateria${numeroGabinetes}`).value;
+    const bateriaInfo = document.getElementById(`bateriaInfo${numeroGabinetes}`);
+    bateriaInfo.style.display = (siteBateria === 'SIM') ? 'block' : 'none';
+}
+
+// Função para mostrar ou esconder o tempo de autonomia
+function mostrarTempoAutonomia(numeroGabinetes) {
+    const autonomia = document.getElementById(`autonomia${numeroGabinetes}`).value;
+    const tempoAutonomiaGroup = document.getElementById(`tempoAutonomiaGroup${numeroGabinetes}`);
+    tempoAutonomiaGroup.style.display = (autonomia === 'SIM') ? 'block' : 'none';
 }
 
 // Função para mostrar ou esconder o modelo do cadeado da entrada do site
@@ -89,7 +115,6 @@ function mostrarModeloCadeadoGradil() {
     const modeloCadeadoGradilGroup = document.getElementById('modeloCadeadoGradilGroup');
     modeloCadeadoGradilGroup.style.display = (gradilCadeado === 'SIM') ? 'block' : 'none';
 }
-
 
 // Função para gerar o relatório
 function gerarRelatorio() {
@@ -132,13 +157,14 @@ function gerarRelatorio() {
             retificadores: document.getElementById(`retificadores${i + 1}`).value,
             baterias: document.getElementById(`baterias${i + 1}`).value,
             siteBateria: document.getElementById(`siteBateria${i + 1}`).value.toUpperCase(),
-            semAutonomia: document.getElementById(`semAutonomia${i + 1}`).checked ? 'SIM' : 'NÃO',
+            semAutonomia: document.getElementById(`autonomia${i + 1}`).value.toUpperCase(),
             infoBateria: document.getElementById(`infoBateria${i + 1}`).value.toUpperCase(),
             quantidadeBancos: document.getElementById(`quantidadeBancos${i + 1}`).value,
             capacidade: document.getElementById(`capacidade${i + 1}`).value.toUpperCase(),
             volts: document.getElementById(`volts${i + 1}`).value.toUpperCase(),
-            elemento: document.getElementById(`elemento${i + 1}`).value.toUpperCase(),
-            consumoFonte: document.getElementById(`consumoFonte${i + 1}`).value.toUpperCase()
+            elemento: document.getElementById(`elemento${i + 1}`).value,
+            consumoFonte: document.getElementById(`consumoFonte${i + 1}`).value,
+            tempoAutonomia: document.getElementById(`tempoAutonomia${i + 1}`).value || ''
         };
         relatorio.gabinetes.push(gabinete);
     }
@@ -160,21 +186,26 @@ function gerarRelatorio() {
 *MODELO DO CADEADO GRADIL:* ${relatorio.modeloCadeadoGradil}
 *SITE VANDALIZADO:* ${relatorio.vandalizado}
 `;
+
     relatorio.gabinetes.forEach((gabinete, index) => {
         resultado += `
 *GABINETE ${index + 1}:* ${gabinete.tipo}
-*QUANTIDADE DE RETIFICADORES:* ${gabinete.retificadores}
-*QUANTIDADE DE BATERIAS:* ${gabinete.baterias}
+*QUANTIDADE DE RETIFICADORES NO GABINETE ${index + 1}-FCC:* ${gabinete.retificadores}
 *SITE COM BATERIA:* ${gabinete.siteBateria}
-*SEM AUTONOMIA:* ${gabinete.semAutonomia}
+${gabinete.siteBateria === 'SIM' ? `
+*QUANTIDADE DE BATERIAS NO GABINETE-FCC ${index + 1}:* ${gabinete.baterias}
 *INFORMAÇÕES DA BATERIA:* ${gabinete.infoBateria}
 *QUANTIDADE DE BANCOS:* ${gabinete.quantidadeBancos}
 *CAPACIDADE:* ${gabinete.capacidade}
 *VOLTS:* ${gabinete.volts}
 *ELEMENTO:* ${gabinete.elemento}
 *CONSUMO FONTE:* ${gabinete.consumoFonte}
-        `;
+*AUTONOMIA:* ${gabinete.semAutonomia}
+${gabinete.semAutonomia === 'SIM' ? `*TEMPO DE AUTONOMIA:* ${gabinete.tempoAutonomia}` : '*SITE SEM AUTONOMIA*'}
+` : '*SEM BATERIA NO GABINETE*'}
+`;
     });
+
     resultado += `
 *SITE POSSUI REDE GPON:* ${relatorio.siteGPON}
 *NECESSÁRIO ZELADORIA:* ${relatorio.zeladoria}
@@ -189,18 +220,11 @@ function gerarRelatorio() {
 *AMI DA PENDÊNCIA:* ${relatorio.amiPendencia}
 *TESTADO COM:* ${relatorio.testadoCom}
 *OBSERVAÇÕES:* ${relatorio.obs}
-    `;
+`;
 
     document.getElementById('resultado').textContent = resultado;
 }
 
-// Função para copiar o relatório para a área de transferência
-function copiarRelatorio() {
-    const resultado = document.getElementById('resultado').textContent;
-    navigator.clipboard.writeText(resultado).then(() => {
-        alert('Relatório copiado para a área de transferência.');
-    });
-}
 
 // Função para enviar o relatório via WhatsApp
 function enviarRelatorio() {
@@ -209,4 +233,13 @@ function enviarRelatorio() {
     window.open(whatsappUrl, '_blank');
 }
 
-;
+// Função para inicializar o formulário
+function inicializarFormulario() {
+    document.getElementById('cadeado').addEventListener('change', mostrarModeloCadeadoEntrada);
+    document.getElementById('gradilCadeado').addEventListener('change', mostrarModeloCadeadoGradil);
+}
+
+window.onload = inicializarFormulario;
+
+// Adicionar evento para o botão de adicionar gabinete
+document.getElementById('adicionarGabineteBtn').addEventListener('click', adicionarGabinete);
